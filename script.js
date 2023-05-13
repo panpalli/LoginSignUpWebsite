1e70a498800d7218db46a6496f8dd950bfaadfee
panpalli <memduhpanpalli@gmail.com>
2023-05-13 21:02:47 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const formOpenBtn = document.querySelector("#form-open"),
+    home = document.querySelector(".home"),
+    formContainer = document.querySelector(".form_container"),
+    formCloseBtn = document.querySelector(".form_close"),
+    singupBtn = document.querySelector("#signup"),
+    loginBtn = document.querySelector("#login"),
+    pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if(getPwInput.type === "password"){
+            getPwInput.type = "text";
+            icon.classList.replace("fa-eye-slash", "fa-eye");
+        }else{
+            getPwInput.type = "password";
+            icon.classList.replace("fa-eye", "fa-eye-slash");
+        }
+    });
+});
+
+singupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
